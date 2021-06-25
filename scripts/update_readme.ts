ab455c9b6d08dec2e1196737c20085daab319bf5
tony <ayakovlenko@users.noreply.github.com>
2021-06-25 18:40:07 +0300

    fix update-readme script

--- scripts/update_readme.ts
+++ scripts/update_readme.ts
@@ -1,12 +1,13 @@
 const latestTag = Deno.args[0];
 if (!latestTag) {
   console.error("tag missing");
+  Deno.exit(1);
 }
 
 const readme = Deno.readTextFileSync("README.md");
 
 const updatedReadme = readme.replaceAll(
-  /git-identity\/.*\//g,
+  /git-identity\/.*?\//g,
   `git-identity/${latestTag}/`,
 );
 
